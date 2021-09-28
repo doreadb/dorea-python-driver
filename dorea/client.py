a727cee545a689279c21a75a63b2c232775e73e6
mrxiaozhuox <mrxzx@wwsg18.top>
2021-09-29 00:24:08 +0800

    commit code

--- dorea/client.py
+++ dorea/client.py
@@ -1,15 +1,33 @@
 from typing import Tuple
+
+import requests
 from dorea import auth
 
 
 class DoreaClient(object):
     def __init__(self, addr: Tuple[str, int], password: str) -> None:
 
-        self.__options = {
-            "addr": addr,
-            "password": password
-        }
-
         url = "http://" + addr[0] + ":" + str(addr[1])
 
-        auth.get_token(url, password)
+        token = auth.get_token(url, password)
+
+        if token == None: 
+            raise ValueError("service password failed")
+
+        self.__options = {
+            "addr": addr,
+            "password": password,
+            "token": token,
+            "url": url
+        }
+
+    def ping(self):
+        
+        r = requests.post(
+            self.__options["url"] + "/ping",
+            headers={ "Authorization": "Bearer " + self.__options["token"]["token"] }
+        )
+        
+        if r.status_code == 200:
+            return True
+        return False
