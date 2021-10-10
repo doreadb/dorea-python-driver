2c752ceac302815a4cbe97c7fa59bdb080b364bb
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-10 17:56:04 +0800

    feat: commit code

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dorea-python-driver',
+    name='pydorea',
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "requests==2.26.0",
         "doson4py==0.1.1"
     ],
-    url='https://github.com/doreadb/dorea-python-driver',
+    url='https://dorea.mrxzx.info/',
     license='MIT',
     author='ZhuoEr Liu',
     author_email='mrxzx.info@gmail.com',
